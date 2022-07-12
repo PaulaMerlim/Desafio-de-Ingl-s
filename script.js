7a3f1dbbc711896450516b398756b699f1a7c9e6
PaulaMerlim <paula.merlim.77@gmail.com>
2022-07-12 12:17:31 -0300

    Desafio de Inlglês em andamento

---
+++ script.js
@@ -0,0 +1,51 @@
+//Crie 3 variáveis, cada uma com um array:
+//- Comedy
+//-Action
+//-Fantasy 
+
+//Dentro de cada array, insira 3 (TRÊS) Objetos, contendo:
+//-title:
+//-director:
+//-year: 
+let Comedy = [{
+    title:'Ice Age',
+    Director:'Chris Wedge',
+    year:'2002'},
+    
+    {
+    title:'The Penguins of Madagascar ',
+    Director:'Eric Darnell and Simon J. Smith',
+    year:'2015'},
+    {
+    title:'Luca ',
+    Director:'Enrico Casarosa',
+    year:'2021'}]
+  
+
+    let Action = [{
+        title:'Cars',
+        Director:'John Lasseter',
+        year:'2006'},
+    {
+        title:' The Incredibles',
+        Director:'John Lasseter',
+        year:'2004'}
+     {       
+        title:'Avengers',
+        Director:' Joss Whedon. Roteiro Joss Whedon and Stan Lee.',
+        year:'2012'
+  
+}]
+let Fantasy = [{
+    title:'Two Brothers',
+    Director:'Jean-Jacques Annaud',
+    year:'2004'},
+{
+    title:'Harry Potter and the Philosophers Stone',
+    Director:'Chris Columbus',
+    year:'2001'}
+ {       
+    title:'Beauty and the Beast',
+    Director:'Gary Trousdale and Kirk Wise',
+    year:'1991'
+}]
